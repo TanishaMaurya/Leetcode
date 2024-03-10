12192dd642c8c037d949e9f97e6bd4b0a1c268c4
Tanisha Maurya <113633939+TanishaMaurya@users.noreply.github.com>
2024-03-10 12:27:13 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (36.23%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,10 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                return i;
+       
+     int ans=searchelement(0,nums.length-1,target,nums);
+        return ans;
+    }
+    public int searchelement(int l,int r,int target,int[] nums){
+        
+        int m=l+(r-l)/2;
+        while(l<=r){
+            if(nums[m]==target){
+                return m;
             }
+        if(nums[m] >target){
+          return  searchelement(l,m-1,target,nums);
+        }
+        else if(nums[m]<target){
+            return searchelement(m+1,r,target,nums);
+        }
         }
         return -1;
     }
