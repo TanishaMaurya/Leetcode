89c60c6067591d02fe83ce0f8d8e2db11d26f1f0
Tanisha Maurya <113633939+TanishaMaurya@users.noreply.github.com>
2024-04-20 10:59:28 +0530

    Time: 19 ms (84.98%), Space: 44.8 MB (91.70%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+     public static int finder(String s,int start,int end,int dp[][]){
+        if(start>end){
+            return dp[start][end]=0;
+        }
+        if(start==end){
+            return dp[start][end]=1;
+        }
+        if(dp[start][end]!=-1){
+            return dp[start][end];
+        }
+        if(s.charAt(start)==s.charAt(end)){
+            return dp[start][end]=2+finder(s,start+1,end-1,dp);
+        }else{
+            return dp[start][end]=0+Math.max(finder(s,start+1,end,dp),finder(s,start,end-1,dp));
+        }
+        
+    }
+    public int longestPalindromeSubseq(String s) {
+        int dp[][]=new int[s.length()][s.length()];
+        for(int i=0;i<dp.length;i++)
+        Arrays.fill(dp[i],-1);
+        return finder(s,0,s.length()-1,dp);
+    
+    }
+    public int minInsertions(String s) {
+      return s.length()-longestPalindromeSubseq(s);  
+    }
+}
